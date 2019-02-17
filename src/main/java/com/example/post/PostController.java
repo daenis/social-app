4ac8e0b5fbf4cc180a940c1f6abfb95104d4cdd4
Dennis Kalaygian <dennis.kalaygian@Trinity.Transport>
2019-02-17 14:39:22 -0500

    Schema

--- src/main/java/com/example/post/PostController.java
+++ src/main/java/com/example/post/PostController.java
@@ -1,6 +1,5 @@
 package com.example.post;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +13,6 @@ public class PostController {
 
     private final PostService postService;
 
-    @Autowired
     public PostController(PostService postService) {
         this.postService = postService;
     }
--- src/main/java/com/example/post/PostServiceImpl.java
+++ src/main/java/com/example/post/PostServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.post;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,7 +10,6 @@ public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
 
-    @Autowired
     public PostServiceImpl(PostRepository postRepository) {
         this.postRepository = postRepository;
     }
